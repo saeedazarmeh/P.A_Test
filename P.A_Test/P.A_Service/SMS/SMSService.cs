48232f6830e2c3cb47d11d9eaa163c5ffd48d280
unknown <saeed_azarmehr@yahoo.com>
2024-09-24 12:56:16 +0100

    Add SMS Succesfully

--- P.A_Test/P.A_Service/SMS/SMSService.cs
+++ P.A_Test/P.A_Service/SMS/SMSService.cs
@@ -9,37 +9,6 @@ namespace P.A_Service.SMS
 {
     public class SMSService
     {
-        public static void SendVerfiedcode(string phoneNumber, string code)
-        {
-            Token smsToken = new Token();
-            var token = smsToken.GetToken("5dffc9923e9763f461c6df18", "@Fartakecu09129176076");
-
-
-            var ultraFastSend = new UltraFastSend()
-            {
-                Mobile = Convert.ToInt64(phoneNumber),
-                TemplateId = 76051,
-                ParameterArray = new List<UltraFastParameters>()
-                {
-                    new UltraFastParameters()
-                    {
-                        Parameter = "VerificationCode" , ParameterValue = code,
-                    },
-                }.ToArray()
-
-            };
-
-            UltraFastSendRespone ultraFastSendRespone = new UltraFast().Send(token, ultraFastSend);
-
-            if (ultraFastSendRespone.IsSuccessful)
-            {
-
-            }
-            else
-            {
-
-            }
-        }
         public static void Sendsuccessfullymessage(string phoneNumber, string firstName, string lasttName)
         {
             Token smsToken = new Token();
